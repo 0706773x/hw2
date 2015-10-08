0e1078435a0d835fdf6122d1427088e65292f94f
Phumz88 <Phumz88@gmail.com>
2015-10-08 12:53:25 +0200

    Final Commit
    hw2

--- pwords.c
+++ pwords.c
@@ -1,29 +1,22 @@
-//PHUMUDZO VUSANI NELUHENI 0706773X
-  //HW2- MULTI-THREADED DICTIONARY
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <pthread.h>
 
-#define MAXWORD 1024
-#define numOfThreads 4  
-
-pthread_mutex_t lock;
-
 typedef struct dict {
   char *word;
   int count;
   struct dict *next;
 } dict_t;
-  dict_t *d;
 
-char *make_word( char *word ) {
+char *
+make_word( char *word ) {
   return strcpy( malloc( strlen( word )+1 ), word );
 }
 
-dict_t *make_dict(char *word) {
+dict_t *
+make_dict(char *word) {
   dict_t *nd = (dict_t *) malloc( sizeof(dict_t) );
   nd->word = make_word( word );
   nd->count = 1;
@@ -31,25 +24,29 @@ dict_t *make_dict(char *word) {
   return nd;
 }
 
-
-dict_t *insert_word( dict_t *d, char *word ) {
+dict_t *
+insert_word( dict_t *d, char *word ) {
+  
+  //   Insert word into dict or increment count if already there
+  //   return pointer to the updated dict
+  
   dict_t *nd;
-  dict_t *pd = NULL; 
-  dict_t *di = d;
-
-  while(di && ( strcmp(word, dictnry->word ) >= 0) ) { 
-    if( strcmp( word, dictnry->word ) == 0 ) { 
-      dictnry->count++;		 
-      return d;		
+  dict_t *pd = NULL;		// prior to insertion point 
+  dict_t *di = d;		// following insertion point
+  // Search down list to find if present or point of insertion
+  while(di && ( strcmp(word, di->word ) >= 0) ) { 
+    if( strcmp( word, di->word ) == 0 ) { 
+      di->count++;		// increment count 
+      return d;			// return head 
     }
-    pd = dictnry;		
-    dictnry = dictnry->next;
+    pd = di;			// advance ptr pair
+    di = di->next;
   }
-  nd = make_dict(word);
-  nd->next = dictnry;		
+  nd = make_dict(word);		// not found, make entry 
+  nd->next = di;		// entry bigger than word or tail 
   if (pd) {
     pd->next = nd;
-    return d;	
+    return d;			// insert beond head 
   }
   return nd;
 }
@@ -61,76 +58,67 @@ void print_dict(dict_t *d) {
   }
 }
 
-int get_word( char *buf, int n, FILE *infile) {
-
+int
+get_word( char *buf, int n, FILE *infile) {
   int inword = 0;
   int c;  
   while( (c = fgetc(infile)) != EOF ) {
     if (inword && !isalpha(c)) {
-      buf[inword] = '\0';
+      buf[inword] = '\0';	// terminate the word string
       return 1;
     } 
     if (isalpha(c)) {
       buf[inword++] = c;
     }
   }
-  return 0;		
+  return 0;			// no more words
 }
 
-void *words( FILE *infile ) {
-  d = NULL;
-  char wordbuf[MAXWORD];
-
-  pthread_mutex_lock(&lock);
-  int have_words = get_word( wordbuf, MAXWORD, infile );
-  pthread_mutex_unlock(&lock);
-
-
-  while( have_words ) {
-	 pthread_mutex_lock(&lock);
- 	// add word to dictinonary
-    	d = insert_word(d, wordbuf); 
-  	pthread_mutex_unlock(&lock);
-	
-	pthread_mutex_lock(&lock);
-	have_words = get_word( wordbuf, MAXWORD, infile );
-	pthread_mutex_unlock(&lock);
+#define MAXWORD 1024
+void *
+words( FILE *infile ) {
+  pthread_mutex_t mutex1;
+  pthread_mutex_t mutex2;
+  dict_t *wd = NULL;
+  char wordbuf[MAXWORD]; 
+  int x = get_word( wordbuf, MAXWORD, infile);;
+  while( x ) {
+    pthread_mutex_lock (&mutex1);
+    wd = insert_word(wd, wordbuf); // add to dict
+    pthread_mutex_unlock (&mutex1);
+    pthread_mutex_lock (&mutex2);
+    x = get_word( wordbuf, MAXWORD, infile);
+    pthread_mutex_unlock (&mutex2);
   }
+  pthread_exit(wd);
 }
 
-int main( int argc, char *argv[] ) {
-
-pthread_t threads[numOfThreads];
-
-	if(pthread_mutex_init(&lock,NULL)!=0)
-	{
-		printf("lock failed");
-	}
-	
-  d = NULL;
+int
+main( int argc, char *argv[] ) {
+  int NTHREADS = 4;
+  pthread_t pth[NTHREADS];
+  void *wd;
+  void *dc[4];
+  int i, j;
+  dict_t *d = NULL;
   FILE *infile = stdin;
-  
-  	if (argc >= 2) {
-    		infile = fopen (argv[1],"r");
-  	}
-  	if( !infile ) {
-    		printf("Unable to open %s\n",argv[1]);
-    		exit( EXIT_FAILURE );
-  	}
+  if (argc >= 2) {
+    infile = fopen (argv[1],"r");
+  }
+  if( !infile ) {
+    printf("Unable to open %s\n",argv[1]);
+    exit( EXIT_FAILURE );
+  }
+  for(i = 0; i<NTHREADS; i++) {
+    pthread_create(&pth[i], NULL, words, infile);
+    pthread_join(pth[i], &wd);
+    dc[i] = wd;
 
-int i;
-for( i = 0; i <= numOfThreads; i++ ){
-	pthread_create( &threads[k], NULL,&words, infile );
+  }
+  for(j = 0; j<NTHREADS; j++){
+  print_dict( dc[j] );
+  }
+  fclose( infile );
 }
 
-int k;
-for( k = 0; k <= numOfThreads; k++ ){
-	pthread_join( threads[j], NULL );
-}
-	print_dict(d);
-	pthread_mutex_destroy(&lock);
-  	fclose( infile );
-}
 
-//PHUMUDZO VUSANI NELUHENI 0706773X
-//HW2- MULTI-THREADED DICTIONARY
